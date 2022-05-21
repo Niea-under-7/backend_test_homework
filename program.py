d4fd3e605c797d57fc3571d71c6a008f714708e9
Alexander Yezhov <yezcode@gmail.com>
2022-05-21 18:44:29 +0300

    My fourth commit

--- program.py
+++ program.py
@@ -1,2 +1,44 @@
-from datetime import timedelta
-print(timedelta.__doc__)
+"""Класс для создания контакта"""
+
+
+class Contact:
+    """Метод, выполняющийся при создании новых контактов"""
+    name: str
+    year_birth: int
+    is_programmer: bool
+
+    def __init__(self,
+                 name: str,
+                 year_birth: int,
+                 is_programmer: bool) -> None:
+        self.name = name
+        self.year_birth = year_birth
+        self.is_programmer = is_programmer
+
+    def age_define(self) -> str:
+        """Определение возраста"""
+        if 1946 < self.year_birth < 1980:
+            return 'Олдскул'
+        if self.year_birth >= 1980:
+            return 'Молодой'
+        return 'Старейшина'
+
+    def programmer_define(self) -> str:
+        """Определение - разработчик или нет"""
+        if self.is_programmer:
+            return 'Программист'
+        return 'Нормальный'
+
+    def show_contact(self) -> str:
+        """Формирование вывода информации"""
+        return (f'{self.name}, '
+                f'категория: {self.age_define()}, '
+                f'статус: {self.programmer_define()}')
+
+    def print_contact(self) -> None:
+        """Вывод контакта"""
+        print(self.show_contact())
+
+
+alex = Contact('Саша', 1990, False)
+alex.print_contact()
